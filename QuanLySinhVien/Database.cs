68c7ed7b3c016d45865229c4092f3d14a0aaf4c3
Nguyen Quang Ha <nguyenquangha295@gmail.com>
2022-04-19 23:06:21 +0700

    Add project files.

---
+++ QuanLySinhVien/Database.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public class Database
+    {
+        private string connetionString = @"Data Source=DESKTOP-SULDBP7\SQLEXPRESS;Initial Catalog = QLSV;User ID = sa; Password = habin2001";
+        private SqlConnection conn;
+        //private string sql;
+        private DataTable dt;
+        private SqlCommand cmd;
+
+        public Database()
+        {
+            try
+            {
+                conn = new SqlConnection(connetionString);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("connected failed: " + ex.Message);
+            }
+
+        }
+        public DataTable SelectData(string sql, List<CustomParameter> lstPara)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                //sql = "exec SelectAllSinhVien";
+                cmd = new SqlCommand(sql, conn);//nội dung sql được truyền vào
+                cmd.CommandType = CommandType.StoredProcedure;//set command type cho cmd
+                foreach (var para in lstPara) //gán các tham số cho cmd
+                {
+                    cmd.Parameters.AddWithValue(para.key, para.value);
+                }
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi load dữ liệu: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public DataRow Select(string sql)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                cmd = new SqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());//thực thi câu lệnh
+                return dt.Rows[0]; //trả về kết quả
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi load thông tin chi tiết: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public int ExeCute(string sql, List<CustomParameter> lstPara)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                foreach (var p in lstPara) //gán các tham số cho cmd
+                {
+                    cmd.Parameters.AddWithValue(p.key, p.value);
+                }
+                var rs = cmd.ExecuteNonQuery();//lấy kết quả thực thi truy vấn
+                return (int)rs; //trả về kết quả
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thực thi câu lệnh: " + ex.Message);
+                return -100;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+
+            }
+        }
+    }
+}
+
