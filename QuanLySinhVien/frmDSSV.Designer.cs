cb97088c3097cd54bc597fd8f463ba2424912ff0
Nguyen Quang Ha <nguyenquangha295@gmail.com>
2022-04-22 09:30:48 +0700

    DSSV

---
+++ QuanLySinhVien/frmDSSV.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace QuanLySinhVien
+{
+    partial class frmDSSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThemmoi = new System.Windows.Forms.Button();
+            this.btnTimkiem = new System.Windows.Forms.Button();
+            this.txtTukhoa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvSinhVien = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThemmoi
+            // 
+            this.btnThemmoi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnThemmoi.Location = new System.Drawing.Point(713, 25);
+            this.btnThemmoi.Name = "btnThemmoi";
+            this.btnThemmoi.Size = new System.Drawing.Size(75, 23);
+            this.btnThemmoi.TabIndex = 8;
+            this.btnThemmoi.Text = "Thêm mới";
+            this.btnThemmoi.UseVisualStyleBackColor = true;
+            this.btnThemmoi.Click += new System.EventHandler(this.btnThemmoi_Click);
+            // 
+            // btnTimkiem
+            // 
+            this.btnTimkiem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTimkiem.Location = new System.Drawing.Point(632, 27);
+            this.btnTimkiem.Name = "btnTimkiem";
+            this.btnTimkiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimkiem.TabIndex = 7;
+            this.btnTimkiem.Text = "Tìm kiếm";
+            this.btnTimkiem.UseVisualStyleBackColor = true;
+            this.btnTimkiem.Click += new System.EventHandler(this.btnTimkiem_Click);
+            // 
+            // txtTukhoa
+            // 
+            this.txtTukhoa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTukhoa.Location = new System.Drawing.Point(441, 27);
+            this.txtTukhoa.Name = "txtTukhoa";
+            this.txtTukhoa.Size = new System.Drawing.Size(185, 20);
+            this.txtTukhoa.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(388, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Từ khóa";
+            // 
+            // dgvSinhVien
+            // 
+            this.dgvSinhVien.AllowUserToAddRows = false;
+            this.dgvSinhVien.AllowUserToDeleteRows = false;
+            this.dgvSinhVien.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSinhVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSinhVien.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvSinhVien.Location = new System.Drawing.Point(0, 80);
+            this.dgvSinhVien.Name = "dgvSinhVien";
+            this.dgvSinhVien.ReadOnly = true;
+            this.dgvSinhVien.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSinhVien.Size = new System.Drawing.Size(800, 370);
+            this.dgvSinhVien.TabIndex = 9;
+            this.dgvSinhVien.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSinhVien_CellDoubleClick);
+            // 
+            // frmDSSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvSinhVien);
+            this.Controls.Add(this.btnThemmoi);
+            this.Controls.Add(this.btnTimkiem);
+            this.Controls.Add(this.txtTukhoa);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDSSV";
+            this.Text = "frmDSSV";
+            this.Load += new System.EventHandler(this.frmDSSV_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhVien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThemmoi;
+        private System.Windows.Forms.Button btnTimkiem;
+        private System.Windows.Forms.TextBox txtTukhoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvSinhVien;
+    }
+}
---
+++ QuanLySinhVien/frmDSSV.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class frmDSSV : Form
+    {
+        public frmDSSV()
+        {
+            InitializeComponent();
+        }
+        private string tukhoa = "";
+
+        private void frmDSSV_Load(object sender, EventArgs e)
+        {
+            LoadDSSV();
+        }
+        private void LoadDSSV()
+        {
+            List<CustomParameter> lstPara = new List<CustomParameter>();
+            lstPara.Add(new CustomParameter()
+            {
+                key = "@tukhoa",
+                value = tukhoa
+            });
+            dgvSinhVien.DataSource = new Database().SelectData("SelectAllSinhVien", lstPara);
+            //đặt tên cột
+            dgvSinhVien.Columns["masinhvien"].HeaderText = "Mã SV";
+            dgvSinhVien.Columns["hoten"].HeaderText = "Họ tên";
+            dgvSinhVien.Columns["nsinh"].HeaderText = "Ngày sinh";
+            dgvSinhVien.Columns["gt"].HeaderText = "Giới tính";
+            dgvSinhVien.Columns["quequan"].HeaderText = "Quê quán";
+            dgvSinhVien.Columns["diachi"].HeaderText = "Địa chỉ";
+            dgvSinhVien.Columns["email"].HeaderText = "Email";
+            dgvSinhVien.Columns["dienthoai"].HeaderText = "Điện thoại";
+        }
+
+        private void dgvSinhVien_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnThemmoi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTimkiem_Click(object sender, EventArgs e)
+        {
+            tukhoa = txtTukhoa.Text;
+            LoadDSSV();
+        }
+    }
+
+}
