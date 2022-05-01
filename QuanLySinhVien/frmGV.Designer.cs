2d8beb0a7a4de085f61238c312b8865b507f3735
duckiman <draballzkai@gmail.com>
2022-05-01 15:14:58 +0700

    form sua cap nhat giao vien

---
+++ QuanLySinhVien/frmGV.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace QuanLySinhVien
+{
+    partial class frmGV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.rbtNu = new System.Windows.Forms.RadioButton();
+            this.rbtNam = new System.Windows.Forms.RadioButton();
+            this.mtbNgaysinh = new System.Windows.Forms.MaskedTextBox();
+            this.txtDienthoai = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtDiachi = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTendem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(282, 304);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(75, 23);
+            this.btnHuy.TabIndex = 46;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(128, 304);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 45;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // rbtNu
+            // 
+            this.rbtNu.AutoSize = true;
+            this.rbtNu.Location = new System.Drawing.Point(283, 148);
+            this.rbtNu.Name = "rbtNu";
+            this.rbtNu.Size = new System.Drawing.Size(39, 17);
+            this.rbtNu.TabIndex = 44;
+            this.rbtNu.Text = "Nữ";
+            this.rbtNu.UseVisualStyleBackColor = true;
+            // 
+            // rbtNam
+            // 
+            this.rbtNam.AutoSize = true;
+            this.rbtNam.Checked = true;
+            this.rbtNam.Location = new System.Drawing.Point(192, 148);
+            this.rbtNam.Name = "rbtNam";
+            this.rbtNam.Size = new System.Drawing.Size(47, 17);
+            this.rbtNam.TabIndex = 43;
+            this.rbtNam.TabStop = true;
+            this.rbtNam.Text = "Nam";
+            this.rbtNam.UseVisualStyleBackColor = true;
+            // 
+            // mtbNgaysinh
+            // 
+            this.mtbNgaysinh.Location = new System.Drawing.Point(177, 117);
+            this.mtbNgaysinh.Mask = "00/00/0000";
+            this.mtbNgaysinh.Name = "mtbNgaysinh";
+            this.mtbNgaysinh.Size = new System.Drawing.Size(100, 20);
+            this.mtbNgaysinh.TabIndex = 42;
+            this.mtbNgaysinh.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtDienthoai
+            // 
+            this.txtDienthoai.Location = new System.Drawing.Point(177, 240);
+            this.txtDienthoai.Name = "txtDienthoai";
+            this.txtDienthoai.Size = new System.Drawing.Size(229, 20);
+            this.txtDienthoai.TabIndex = 39;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(177, 210);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(229, 20);
+            this.txtEmail.TabIndex = 38;
+            // 
+            // txtDiachi
+            // 
+            this.txtDiachi.Location = new System.Drawing.Point(177, 179);
+            this.txtDiachi.Name = "txtDiachi";
+            this.txtDiachi.Size = new System.Drawing.Size(229, 20);
+            this.txtDiachi.TabIndex = 37;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(177, 91);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(229, 20);
+            this.txtTen.TabIndex = 35;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Giới tính";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Ngày sinh";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(64, 243);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "Điện thoại";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(64, 213);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(64, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Địa chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Tên";
+            // 
+            // txtTendem
+            // 
+            this.txtTendem.Location = new System.Drawing.Point(177, 65);
+            this.txtTendem.Name = "txtTendem";
+            this.txtTendem.Size = new System.Drawing.Size(229, 20);
+            this.txtTendem.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Tên đệm";
+            // 
+            // txtHo
+            // 
+            this.txtHo.Location = new System.Drawing.Point(177, 39);
+            this.txtHo.Name = "txtHo";
+            this.txtHo.Size = new System.Drawing.Size(229, 20);
+            this.txtHo.TabIndex = 36;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Họ";
+            // 
+            // frmGV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 382);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.rbtNu);
+            this.Controls.Add(this.rbtNam);
+            this.Controls.Add(this.mtbNgaysinh);
+            this.Controls.Add(this.txtDienthoai);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtDiachi);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTendem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHo);
+            this.Controls.Add(this.label1);
+            this.Name = "frmGV";
+            this.Text = "frmGV";
+            this.Load += new System.EventHandler(this.frmGV_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.RadioButton rbtNu;
+        private System.Windows.Forms.RadioButton rbtNam;
+        private System.Windows.Forms.MaskedTextBox mtbNgaysinh;
+        private System.Windows.Forms.TextBox txtDienthoai;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtDiachi;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTendem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QuanLySinhVien/frmGV.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class frmGV : Form
+    {
+        public frmGV(string mgv)
+        {
+            this.mgv = mgv;
+            InitializeComponent();
+        }
+        private string mgv;
+        private string nguoithucthi = "admin";
+
+        private void frmGV_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(mgv))
+            {
+                this.Text = "Thêm mới giáo viên";
+            }
+            else
+            {
+                this.Text = "Cập nhạt giáo viên";
+                var r = new Database().Select("selectGV'" + int.Parse(mgv) + "'");
+                txtHo.Text = r["ho"].ToString();
+                txtTendem.Text = r["tendem"].ToString();
+                txtTen.Text = r["ten"].ToString();
+                rbtNam.Checked = r["gioitinh"].ToString() == "1" ? true : false;
+                mtbNgaysinh.Text = r["ngsinh"].ToString();
+                txtDienthoai.Text = r["dienthoai"].ToString();
+                txtEmail.Text = r["email"].ToString();
+                txtDiachi.Text = r["diachi"].ToString();
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+       
+}
