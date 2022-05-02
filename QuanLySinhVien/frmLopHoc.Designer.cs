46a8acb265827bb10c78dfbd803cf3267f353a0f
PhamDuc1211 <92295467+PhamDuc1211@users.noreply.github.com>
2022-05-02 22:18:54 +0700

    tao form lophoc

---
+++ QuanLySinhVien/frmLopHoc.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace QuanLySinhVien
+{
+    partial class frmLopHoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.cbbGiaoVien = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbMonhoc = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(332, 103);
+            this.btnHuy.Margin = new System.Windows.Forms.Padding(4);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(100, 28);
+            this.btnHuy.TabIndex = 7;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(188, 103);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(100, 28);
+            this.btnLuu.TabIndex = 8;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // cbbGiaoVien
+            // 
+            this.cbbGiaoVien.FormattingEnabled = true;
+            this.cbbGiaoVien.Location = new System.Drawing.Point(161, 57);
+            this.cbbGiaoVien.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbGiaoVien.Name = "cbbGiaoVien";
+            this.cbbGiaoVien.Size = new System.Drawing.Size(432, 24);
+            this.cbbGiaoVien.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Giáo viên";
+            // 
+            // cbbMonhoc
+            // 
+            this.cbbMonhoc.FormattingEnabled = true;
+            this.cbbMonhoc.Location = new System.Drawing.Point(161, 23);
+            this.cbbMonhoc.Margin = new System.Windows.Forms.Padding(4);
+            this.cbbMonhoc.Name = "cbbMonhoc";
+            this.cbbMonhoc.Size = new System.Drawing.Size(432, 24);
+            this.cbbMonhoc.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Môn học";
+            // 
+            // frmLopHoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 154);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.cbbGiaoVien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbMonhoc);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLopHoc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLopHoc";
+            this.Load += new System.EventHandler(this.frmLopHoc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.ComboBox cbbGiaoVien;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbMonhoc;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QuanLySinhVien/frmLopHoc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class frmLopHoc : Form
+    {
+        public frmLopHoc(string malophoc)
+        {
+            this.malophoc = malophoc;
+            InitializeComponent();
+        }
+        private string malophoc;
+        private Database db;
+        private string nguoithuchien = "admin";
+
+        private void frmLopHoc_Load(object sender, EventArgs e)
+        {
+            db = new Database();
+            List<CustomParameter> lst = new List<CustomParameter>()
+            {
+                new CustomParameter()
+                {
+                    key = "@tukhoa",
+                    value = ""
+                }
+            };
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
