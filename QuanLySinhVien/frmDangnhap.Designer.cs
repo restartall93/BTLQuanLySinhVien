1eea1a986caf1a195543f18932de80cba0adcecc
duckiman <draballzkai@gmail.com>
2022-05-06 10:52:24 +0700

    form dangnhap

---
+++ QuanLySinhVien/frmDangnhap.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace QuanLySinhVien
+{
+    partial class frmDangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.txtTendangnhap = new System.Windows.Forms.TextBox();
+            this.cbbLoaiTaiKhoan = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(443, 255);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 13;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(362, 255);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 12;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(389, 215);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.Size = new System.Drawing.Size(199, 20);
+            this.txtMatkhau.TabIndex = 11;
+            this.txtMatkhau.UseSystemPasswordChar = true;
+            // 
+            // txtTendangnhap
+            // 
+            this.txtTendangnhap.Location = new System.Drawing.Point(389, 175);
+            this.txtTendangnhap.Name = "txtTendangnhap";
+            this.txtTendangnhap.Size = new System.Drawing.Size(199, 20);
+            this.txtTendangnhap.TabIndex = 10;
+            // 
+            // cbbLoaiTaiKhoan
+            // 
+            this.cbbLoaiTaiKhoan.FormattingEnabled = true;
+            this.cbbLoaiTaiKhoan.Items.AddRange(new object[] {
+            "Quản trị viên",
+            "Giáo viên",
+            "Sinh viên"});
+            this.cbbLoaiTaiKhoan.Location = new System.Drawing.Point(389, 133);
+            this.cbbLoaiTaiKhoan.Name = "cbbLoaiTaiKhoan";
+            this.cbbLoaiTaiKhoan.Size = new System.Drawing.Size(199, 21);
+            this.cbbLoaiTaiKhoan.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mật khẩu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tên đăng nhập";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Loại tài khoản";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // frmDangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 339);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtMatkhau);
+            this.Controls.Add(this.txtTendangnhap);
+            this.Controls.Add(this.cbbLoaiTaiKhoan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDangnhap";
+            this.Text = "frmDangnhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.TextBox txtTendangnhap;
+        private System.Windows.Forms.ComboBox cbbLoaiTaiKhoan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ QuanLySinhVien/frmDangnhap.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class frmDangnhap : Form
+    {
+        public frmDangnhap()
+        {
+            InitializeComponent();
+        }
+        private string tendangnhap;
+        public string loaitk;
+       
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            #region ktra_rangbuoc
+            if (cbbLoaiTaiKhoan.SelectedIndex <0)
+            {
+                MessageBox.Show("vui lòng chọn loại tài khoản");
+                return;
+            }    
+
+           if(string.IsNullOrEmpty(txtTendangnhap.Text))
+            {
+                MessageBox.Show("Vui lòng đăng nhập tài khoản", "Tài khoản không được để trống");
+                txtTendangnhap.Select();
+                return;
+            }   
+           
+           if(string.IsNullOrEmpty(txtMatkhau.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mật khẩu", "Mật khẩu không để trống");
+            }
+            #endregion
+
+            tendangnhap = txtTendangnhap.Text;
+            loaitk = "";
+
+            switch (cbbLoaiTaiKhoan.Text)
+            {
+                case "Quản trị viên":
+                    loaitk = "admin";
+                    break;
+                case "Giáo viên":
+                    loaitk = "gv";
+                    break;
+                case "Sinh viên":
+                    loaitk = "sv";
+                    break;
+            }
+
+            List<CustomParameter> lst = new List<CustomParameter>()
+            {
+                new CustomParameter()
+                {
+                    key = "@loaitaikhoan",
+                    value=loaitk
+                },
+                new CustomParameter()
+                {
+                    key = "@taikhoan",
+                    value=txtTendangnhap.Text
+                },
+                new CustomParameter()
+                {
+                    key ="@matkhau",
+                    value=txtMatkhau.Text
+                },
+            };
+
+            var rs = new Database().SelectData("dangnhap", lst);
+            if(rs.Rows.Count > 0)
+            {
+                this.Hide();
+            }    
+            else
+            {
+                MessageBox.Show("Vui lòng kiếm tra lại tên đăng nhập hoặc mật khẩu", "Tài khoản không hợp lệ");
+
+            }    
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
