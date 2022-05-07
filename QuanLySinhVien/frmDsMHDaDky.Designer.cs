89d97197de4d41755e47a81f929d53ae88845af1
chien67 <chien672001@gmail.com>
2022-05-07 23:46:21 +0700

    frm DsMHDKy

---
+++ QuanLySinhVien/frmDsMHDaDky.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace QuanLySinhVien
+{
+    partial class frmDsMHDaDky
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDkyMoi = new System.Windows.Forms.Button();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.txtTimkiem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvDSMHDky = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSMHDky)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDkyMoi
+            // 
+            this.btnDkyMoi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDkyMoi.Location = new System.Drawing.Point(889, 13);
+            this.btnDkyMoi.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDkyMoi.Name = "btnDkyMoi";
+            this.btnDkyMoi.Size = new System.Drawing.Size(165, 28);
+            this.btnDkyMoi.TabIndex = 6;
+            this.btnDkyMoi.Text = "Đăng ký mới";
+            this.btnDkyMoi.UseVisualStyleBackColor = true;
+            this.btnDkyMoi.Click += new System.EventHandler(this.btnDkyMoi_Click);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTimKiem.Location = new System.Drawing.Point(781, 13);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(100, 28);
+            this.btnTimKiem.TabIndex = 7;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // txtTimkiem
+            // 
+            this.txtTimkiem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTimkiem.Location = new System.Drawing.Point(450, 16);
+            this.txtTimkiem.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTimkiem.Name = "txtTimkiem";
+            this.txtTimkiem.Size = new System.Drawing.Size(321, 22);
+            this.txtTimkiem.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(380, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Từ khóa";
+            // 
+            // dgvDSMHDky
+            // 
+            this.dgvDSMHDky.AllowUserToAddRows = false;
+            this.dgvDSMHDky.AllowUserToDeleteRows = false;
+            this.dgvDSMHDky.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDSMHDky.Location = new System.Drawing.Point(24, 48);
+            this.dgvDSMHDky.Name = "dgvDSMHDky";
+            this.dgvDSMHDky.ReadOnly = true;
+            this.dgvDSMHDky.RowHeadersWidth = 51;
+            this.dgvDSMHDky.RowTemplate.Height = 24;
+            this.dgvDSMHDky.Size = new System.Drawing.Size(1031, 494);
+            this.dgvDSMHDky.TabIndex = 8;
+            // 
+            // frmDsMHDaDky
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.dgvDSMHDky);
+            this.Controls.Add(this.btnDkyMoi);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTimkiem);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDsMHDaDky";
+            this.Text = "Danh Sách Môn Học Đã Đăng Ký";
+            this.Load += new System.EventHandler(this.frmDsMHDaDky_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSMHDky)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDkyMoi;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.TextBox txtTimkiem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvDSMHDky;
+    }
+}
---
+++ QuanLySinhVien/frmDsMHDaDky.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class frmDsMHDaDky : Form
+    {
+        private string masv;
+        public frmDsMHDaDky(string masv)
+        {
+            this.masv = masv;
+            InitializeComponent();
+        }
+
+        private void frmDsMHDaDky_Load(object sender, EventArgs e)
+        {
+            LoadMonDk();
+        }
+        private void LoadMonDk()
+        {
+            List<CustomParameter> lst = new List<CustomParameter>()
+            {
+                new CustomParameter()
+                {
+                    key = "@masinhvien",
+                    value = masv
+                }
+            };
+            dgvDSMHDky.DataSource = new Database().SelectData("monDaDKy", lst);
+        }
+
+        private void btnDkyMoi_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
