870791840e5e67ac6d89c947b0d24494ee1d6c8d
chien67 <chien672001@gmail.com>
2022-05-11 00:07:16 +0700

    Update frmDangkyMonhoc.cs

--- QuanLySinhVien/frmDangkyMonhoc.cs
+++ QuanLySinhVien/frmDangkyMonhoc.cs
@@ -40,7 +40,36 @@ namespace QuanLySinhVien
 
         private void dgvDSLH_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
+            if(dgvDSLH.Rows[e.RowIndex].Index >= 0)
+            {
+                if(DialogResult.Yes == MessageBox.Show("Bạn muốn đăng ký học phần [" + dgvDSLH.Rows[e.RowIndex].Cells["tenmonhoc"].Value.ToString()+ "]?",
+                                                       "Xác nhận đăng ký", MessageBoxButtons.YesNo,MessageBoxIcon.Question ))
+                {
+                    List<CustomParameter> lstPara = new List<CustomParameter>();
+                    lstPara.Add(new CustomParameter()
+                    {
+                        key = "@masinhvien",
+                        value = msv
+                    });
+                    lstPara.Add(new CustomParameter()
+                    {
+                        key = "@malophoc",
+                        value = dgvDSLH.Rows[e.RowIndex].Cells["malophoc"].Value.ToString()
+                    });
 
+                    var rs = new Database().ExeCute("[dkyhoc]", lstPara);
+                    if(rs == -1)
+                    {
+                        MessageBox.Show("Học phần này bạn đã đăng ký", "Cảnh báo!!!!!");
+                        return;
+                    }
+                    if(rs == 1)
+                    {
+                        MessageBox.Show("Đã đăng ký học phần thành công", "SUCCESS!!!!!!");
+                        LoadDSLH();
+                    }
+                }
+            }
         }
     }
 }
