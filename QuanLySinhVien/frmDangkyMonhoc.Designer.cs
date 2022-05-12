fe414550eef40596c02a831a7c367d82dd5e8aff
PhamDuc1211 <92295467+PhamDuc1211@users.noreply.github.com>
2022-05-12 22:47:47 +0700

    Update frmDangkyMonhoc.Designer.cs

--- QuanLySinhVien/frmDangkyMonhoc.Designer.cs
+++ QuanLySinhVien/frmDangkyMonhoc.Designer.cs
@@ -40,10 +40,12 @@
             this.dgvDSLH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDSLH.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvDSLH.Location = new System.Drawing.Point(0, 0);
+            this.dgvDSLH.MultiSelect = false;
             this.dgvDSLH.Name = "dgvDSLH";
             this.dgvDSLH.ReadOnly = true;
             this.dgvDSLH.RowHeadersWidth = 51;
             this.dgvDSLH.RowTemplate.Height = 24;
+            this.dgvDSLH.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvDSLH.Size = new System.Drawing.Size(800, 450);
             this.dgvDSLH.TabIndex = 0;
             this.dgvDSLH.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDSLH_CellDoubleClick);
@@ -55,6 +57,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dgvDSLH);
             this.Name = "frmDangkyMonhoc";
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Danh sach lop hoc";
             this.Load += new System.EventHandler(this.frmDangkyMonhoc_Load);
